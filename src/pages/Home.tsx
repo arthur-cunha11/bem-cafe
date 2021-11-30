e1482131baa8b75e0e5d75a71827f167c1e44727
Arthur <arthur.cunha@acct.global>
2021-11-30 02:20:31 -0300

    creating project structure

---
+++ src/pages/Home.tsx
@@ -0,0 +1,5 @@
+import "../styles/home.scss";
+
+export const Home = () => {
+  return <main id="home">abc</main>;
+};
---
+++ src/services/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDd-HR9cUK3opGxcDJ9jQ_uz191hJlzBbs",
+  authDomain: "bem-cafe.firebaseapp.com",
+  databaseURL: "https://bem-cafe-default-rtdb.firebaseio.com",
+  projectId: "bem-cafe",
+  storageBucket: "bem-cafe.appspot.com",
+  messagingSenderId: "34704800958",
+  appId: "1:34704800958:web:b6360fa44790f2835cb2ef",
+};
+
+initializeApp(firebaseConfig);
